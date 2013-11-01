87f81c7390a7ab969b7b3c53ebc6fd94f59b7d29
Ilya Mitin <rumterg@gmail.com>
2013-11-01 19:49:18 +0400

    отладка дорожек

--- src/main/java/org/rumter/chj/models/StudentCity/ground/Ground.java
+++ src/main/java/org/rumter/chj/models/StudentCity/ground/Ground.java
@@ -27,7 +27,7 @@ public class Ground extends SimpleModel {
 
 	@Override
 	public void display() {
-		App.texUtils.prepareForDisplay("ground/ground_1");
+		App.texUtils.prepareForDisplay("ground/ground_");
 		App.drawUtils.drawQuadTex(q);
 		trackSet.display();
 	}
